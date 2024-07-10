4247da3c262f855507296e763fdf4f2c07b9159d
Maksymilian Wilczyński <makswilczynski2010@gmail.com>
2024-07-10 20:29:47 +0200

    make the build script better

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
+@echo off
 title Building source engine
-echo "Building day of defeat x64"
-waf.bat configure -T release --prefix=dod --build-games=dod --disable-warns --enable-opus
-waf.bat build -v -p
-waf.bat install
+SET GAME=dod
+SET OPUS=true
+SET PRESET=release
+SET PREFIX=dod
+if %GAME%==dod (if %OPUS%==true (echo Building day of defeat x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
+if %GAME%==dod (if %OPUS%==false (echo Building day of defeat x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
 echo "Building completed"
-echo "Binary location: dod"
+if %COMPILED%==true (echo Binary location: %PREFIX%) else echo Did not compile
+pause
