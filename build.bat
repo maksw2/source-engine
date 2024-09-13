091faa2894e8f4be6ccbb8e3acc43a4ca3981410
Maksymilian Wilczyński <makswilczynski2010@gmail.com>
2024-09-13 19:09:25 +0200

    change default game to hl2mp

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 title Building source engine
-SET GAME=dod
+SET GAME=hl2mp
 SET OPUS=true
 SET PRESET=release
-SET PREFIX=dod
-if %GAME%==dod (if %OPUS%==true (echo Building day of defeat x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
-if %GAME%==dod (if %OPUS%==false (echo Building day of defeat x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
+SET PREFIX=hl2mp
+if %GAME%==hl2mp (if %OPUS%==true (echo Building hl2mp x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
+if %GAME%==hl2mp (if %OPUS%==false (echo Building hl2mp x64 && python3 waf configure -T %PRESET% --prefix=%PREFIX% --build-games=%GAME% --disable-warns --enable-opus && python3 waf build -v -p && python3 waf install)) else echo Script error! && SET COMPILED=false
 echo "Building completed"
 if %COMPILED%==true (echo Binary location: %PREFIX%) else echo Did not compile
 pause
